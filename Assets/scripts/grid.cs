cb92f8309025d30c51315ab78bed01116f9e935d
Brian Kang <1kangyun1@gmail/com>
2019-09-28 01:59:33 -0400

    Extra files for recompling issues.

--- Assets/scripts/grid.cs
+++ Assets/scripts/grid.cs
@@ -6,10 +6,12 @@ using UnityEngine;
 public class grid : MonoBehaviour
 {
 
-    public float cell_size = 0.5f; // = larghezza/altezza delle celle
+    public float cell_size = 0.64f;
     public Renderer rend;
     public int posX, posY;
-    private float x, y, z;
+    private float x, y, z, r;
+    public grid[] nearTiles;
+    public bool start = true;
 
     void Start()
     {
@@ -18,10 +20,14 @@ public class grid : MonoBehaviour
         z = 0f;
         rend = GetComponent<Renderer>();
 
+        r = cell_size;
+
+        
     }
 
     void Update()
     {
+        //Move to Start() after map is finalized.
         x = Mathf.Round(transform.position.x / cell_size) * cell_size;
         y = Mathf.Round(transform.position.y / cell_size) * cell_size;
         z = transform.position.z;
@@ -29,6 +35,28 @@ public class grid : MonoBehaviour
 
         posX = (int)Mathf.Round(x / 0.64f);
         posY = (int)Mathf.Round(y / -0.64f);
+
+        if (start)
+        {
+            start = false;
+
+            Collider2D[] nearColliders = Physics2D.OverlapCircleAll(new Vector3(x, y, z), r);
+            int i = 0;
+            int j = 0;
+            nearTiles = new grid[nearColliders.Length - 1];
+            while (i < nearColliders.Length)
+            {
+                if (!(nearColliders[i].gameObject == this.gameObject))
+                {
+                    nearTiles[i - j] = nearColliders[i].GetComponent<grid>();
+                }
+                else
+                {
+                    j++;
+                }
+                i++;
+            }
+        }
     }
 
     void OnMouseEnter()
