32dae4167f3aeb0db2b0d2574c0b2dd627783be4
Ditto000 <richard.0125@hotmail.com>
2019-09-28 17:41:58 -0400

    Walking AI first implementation

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -1,18 +1,45 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private Tile[] spawnPoints;
+    public TileManager grid;
+    public Entity aHero;
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawnPoints = new Tile[3];
+        //grid = GameObject.Find("gridTiles");
+        grid.populateTiles();
+        generateSpawnArray();
+        //Debug.Log(spawnPoints[0].posX);
+        //Debug.Log(spawnPoints[0].posY);
+        Instantiate(aHero, spawnPoints[1].transform.position, Quaternion.identity);
+        aHero.spawn(spawnPoints[1], 1, 1, 1, 80);
+    }
+
+    private void generateSpawnArray()
+    {
+        int spawnIndex = 0;
+        for (int i = 0; i < grid.width; i++)
+        {
+            for (int j = 0; j < grid.height; j++)
+            {
+                if (grid.tiles[i, j].tag == "summon")
+                {
+                    spawnPoints[spawnIndex] = grid.tiles[i, j];
+                    spawnIndex++;
+                }
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
