324e8958fb7e6380db362b2f92fb4b5b09152db4
Ditto000 <richard.0125@hotmail.com>
2019-09-28 17:41:58 -0400

    Walking AI first implementation

--- Assets/scripts/TileManager.cs
+++ Assets/scripts/TileManager.cs
@@ -10,25 +10,29 @@ public class TileManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        tiles = new Tile[width, height];
-        Tile temp;
-        int i = 0;
 
-        Collider2D[] nearColliders = Physics2D.OverlapBoxAll(new Vector2(0,0),new Vector2(80,80),0);
-        while (i < nearColliders.Length)
-        {
-            if (!(nearColliders[i].gameObject == tilePrefab.gameObject))
-            {
-                temp = nearColliders[i].GetComponent<Tile>();
-                tiles[temp.posX,temp.posY] = temp;
-            }
-            i++;
-        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void populateTiles()
+    {
+        tiles = new Tile[width, height];
+        Tile temp;
+        int i = 0;
+
+        Collider2D[] nearColliders = Physics2D.OverlapBoxAll(new Vector2(0, 0), new Vector2(80, 80), 0);
+        while (i < nearColliders.Length)
+        {
+            if (!(nearColliders[i].gameObject == tilePrefab.gameObject))
+            {
+                temp = nearColliders[i].GetComponent<Tile>();
+                tiles[temp.posX, temp.posY] = temp;
+            }
+            i++;
+        }
     }
 }
